f0800708d243aa12c05678097b7f18102a85fd57
Enrique <100406632@alumnos.uc3m.es>
2020-08-05 20:30:14 +0200

    feat: Designed activity_puzzle.xml

--- app/src/main/java/com/example/android/lateralthinking/MainActivity.java
+++ app/src/main/java/com/example/android/lateralthinking/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.lateralthinking;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void startQuiz (View view){
+        Intent i = new Intent(this, PuzzleActivity.class);
+        startActivity(i);
     }
 }
